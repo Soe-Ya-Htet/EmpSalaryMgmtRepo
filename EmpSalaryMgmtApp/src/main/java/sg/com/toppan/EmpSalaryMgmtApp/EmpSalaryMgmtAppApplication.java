7a2e04289df8bd94ed60490a14f2b6539eed0308
Soe Ya Htet <shakusoe@gmail.com>
2022-11-28 20:12:44 +0800

    App - Spring Boot app first commit.

---
+++ EmpSalaryMgmtApp/src/main/java/sg/com/toppan/EmpSalaryMgmtApp/EmpSalaryMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package sg.com.toppan.EmpSalaryMgmtApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSalaryMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSalaryMgmtAppApplication.class, args);
+	}
+
+}
---
+++ EmpSalaryMgmtApp/src/test/java/sg/com/toppan/EmpSalaryMgmtApp/EmpSalaryMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.toppan.EmpSalaryMgmtApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpSalaryMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
