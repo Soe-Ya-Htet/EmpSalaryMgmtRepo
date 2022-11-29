7ef30feab32026d46febc4d76540ba4bd69e743f
Soe Ya Htet <shakusoe@gmail.com>
2022-11-29 23:12:59 +0800

    Web - add employee-search component and service

--- EmpSalaryMgmtWeb/src/app/app.module.ts
+++ EmpSalaryMgmtWeb/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeSearchComponent } from './employee-search/employee-search.component';
+import { EmployeeService } from './employee.service';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
